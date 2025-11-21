ba64b593037fae29b19a0ab533f686e7cce19df7
Tudor Raul Ciprian <78701525+raulciprian-tudor@users.noreply.github.com>
2025-11-21 15:34:38 +0100

    build fixes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,10 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'project/:id',
+    renderMode: RenderMode.Server,
+
   }
 ];
